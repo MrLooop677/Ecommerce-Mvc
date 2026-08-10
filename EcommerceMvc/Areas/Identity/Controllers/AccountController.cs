cba0e25da5876973e315c421a190244c1fc96a14
eslam mahmoud <eslam.mahmoud@watanfd.com>
2026-08-11 02:20:47 +0300

    init register form

---
+++ EcommerceMvc/Areas/Identity/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceMvc.Areas.Identity.Controllers
+{
+    [Area("Identity")]
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
